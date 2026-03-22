f25366f8bdcc2b0e9f9e4f304cad15ef9eef68dc
Najmul Akash <najmulbalghari23@gmail.com>
2026-03-21 21:15:27 -0700

    task

--- Datalakaanahabackendsa.js
+++ Datalakaanahabackendsa.js
@@ -17,17 +17,15 @@ const Server = http.createServer((req, res)=>{
     res.write('<html>')
     res.write('<head><title>Node JS</title></head>')
     res.write('<body><h1>Wellcome</h1></body>')
+    res.write('<form>')
+    res.write('<input type="text" name="Username" placeholder="Enter your Details">')
+      res.write('<input type="text" name="UserEmail" placeholder="Enter your Details">')
+      res.write('<submit type="submit" value="submit">')
+    res.write('</form>')
     res.write('</html>')
    return res.end()
     }
-    else if(req.url==='/About'){
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>Node JS</title></head>')
-    res.write('<body><h1>Node Products</h1></body>')
-    res.write('</html>')
-   return res.end()
-    }
+ 
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title>Node JS</title></head>')
@@ -41,4 +39,9 @@ Server.listen(port,()=>{
     console.log(`server runing at http://localhost:${port}`
         
     )
+    // console.log("najmul akash")
+    // console.log("Print ffff")
+    // console.log("function 1")
+    // console.log("fuction 2")
+    // console.log("fuction 3")
 })   
---
+++ Httpcreate.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const Server=http.createServer((req, res)=>{
+    console.log(req)
+});
+const PORT=3001;
+Server.listen(PORT,()=>{
+    console.log(`Server running on address http://localhost:${PORT}`);
+})
